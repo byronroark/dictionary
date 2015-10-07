9d7a11aa4a9f00d2251d92b239973028f5c72164
Byron Roark <byronroark@gmail.com>
2015-10-07 01:12:30 -0400

    ran users migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150930034745) do
+ActiveRecord::Schema.define(version: 20151007041118) do
 
   create_table "definitions", force: :cascade do |t|
     t.string   "word"
     t.string   "type"
     t.text     "meaning"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
