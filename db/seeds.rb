62cf22342e8ee783309d11c71f06bc8797a27fbe
Byron Roark <byronroark@gmail.com>
2015-10-09 17:09:50 -0400

    Saves records in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-User.new(email: 'byronroark@gmail.com', password: 'password')
-User.new(email: 'gavin@example.com', password: 'sekret')
-User.new(email: 'jason@example.com', password: 'alsosekret')
-User.new(email: 'toni@example.com', password: 'supersekret')
+User.create(email: 'byronroark@gmail.com', password: 'password')
+User.create(email: 'gavin@example.com', password: 'sekret')
+User.create(email: 'jason@example.com', password: 'alsosekret')
+User.create(email: 'toni@example.com', password: 'supersekret')
