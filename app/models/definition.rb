b6b2756d3655af735c8d8f253065cbd45883138c
Byron Roark <byronroark@gmail.com>
2015-10-21 17:11:27 -0400

    added validation for uploading image type

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -2,7 +2,7 @@ class Definition < ActiveRecord::Base
   validates :word, presence: true, uniqueness: true
   validates :meaning, presence: true
 
-  attachment :profile_image
+  attachment :profile_image, type: :image
 
   def self.search(search)
     where("word like ? or meaning like ?", "%#{search}%", "%#{search}%")
